cb55098e55bd4b238b8e53ca2db9d926cefa7f01
강원준 <devsigner9920@icloud.com>
2024-11-17 22:20:22 +0900

    Time: 6 ms (63.01%), Space: 45.9 MB (66.67%) - LeetHub

--- 2639-find-the-width-of-columns-of-a-grid/2639-find-the-width-of-columns-of-a-grid.java
+++ 2639-find-the-width-of-columns-of-a-grid/2639-find-the-width-of-columns-of-a-grid.java
@@ -8,7 +8,7 @@ class Solution {
         for (int i = 0; i < n; i++) {
             var temp = -1000000001;
             for (int j = 0; j < m; j++) {
-                temp = Math.max(temp, (grid[j][i] + "").length());
+                temp = Math.max(temp, String.valueOf(grid[j][i]).length());
             }
             arr[i] = temp;
         }
