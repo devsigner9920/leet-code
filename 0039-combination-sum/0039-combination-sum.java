844a7e9318b575fdf05838afc1e73597882103f9
강원준 <devsigner9920@icloud.com>
2024-11-22 23:45:48 +0900

    Time: 1014 ms (5.25%), Space: 53.3 MB (6.24%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        var result = new ArrayList<List<Integer>>();
+        backtracking(result, 0, new ArrayList<Integer>(), candidates, target);
+        return result.stream().distinct().collect(Collectors.toList());
+    }
+
+    public void backtracking(List<List<Integer>> result, int start, List<Integer> temp, int[] candidates, int target) {
+        var res = temp.stream().mapToInt(Integer::intValue).sum();
+        if (res > target) {
+            return;
+        }
+
+        if (res == target) {
+            var integers = new ArrayList<>(temp).stream().sorted().collect(Collectors.toList());
+            result.add(integers);
+            return;
+        }
+
+        for (int i = 0; i < candidates.length; i++) {
+            temp.add(candidates[i]);
+            backtracking(result, i, temp, candidates, target);
+             temp.remove(temp.size() - 1);
+        }
+    }
+}
