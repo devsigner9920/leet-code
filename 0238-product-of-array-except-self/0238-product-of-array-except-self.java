342852b22fb269860aba1367f8d782ceded29229
강원준 <devsigner9920@icloud.com>
2024-12-28 21:54:05 +0900

    Time: 2 ms (89.37%), Space: 55.2 MB (76.81%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,13 +1,15 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        var result = new int[nums.length];
+        int[] result = new int[nums.length];
 
         result[0] = 1;
-        for (int i = 1; i < nums.length; i++) {
+
+        for (int i = 1; i < result.length; i++) {
             result[i] = nums[i - 1] * result[i - 1];
         }
 
-        var acc = 1;
+        int acc = 1;
+
         for (int i = nums.length - 2; i >= 0; i--) {
             acc *= nums[i + 1];
             result[i] *= acc;
