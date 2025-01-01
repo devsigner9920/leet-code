683b21ff7f690847a9bbeb751ed01c74f6e32e49
강원준 <devsigner9920@icloud.com>
2025-01-01 18:14:19 +0900

    Time: 1 ms (5.65%), Space: 42.1 MB (22.37%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == val) {
+                count++;
+                nums[i] = 1000;
+            }
+        }
+
+        Arrays.sort(nums);
+
+        return nums.length - count;
+    }
+}
