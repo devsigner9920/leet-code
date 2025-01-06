b7c18e248b0c902fa2aacdbd102716be39e7b179
강원준 <devsigner9920@icloud.com>
2025-01-06 19:46:58 +0900

    Time: 8 ms (56.25%), Space: 44.6 MB (77.78%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,14 +1,32 @@
 class Solution {
     public int[] minOperations(String boxes) {
-        // bruteforce
         int[] answer = new int[boxes.length()];
 
-        for (int currentBox = 0; currentBox < boxes.length(); currentBox++) {
-            if (boxes.charAt(currentBox) == '1') {
-                for (int newPosition = 0; newPosition < boxes.length(); newPosition++) {
-                    answer[newPosition] += Math.abs(newPosition - currentBox);
-                }
-            }
+        // bruteforce
+        // for (int currentBox = 0; currentBox < boxes.length(); currentBox++) {
+        //     if (boxes.charAt(currentBox) == '1') {
+        //         for (int newPosition = 0; newPosition < boxes.length(); newPosition++) {
+        //             answer[newPosition] += Math.abs(newPosition - currentBox);
+        //         }
+        //     }
+        // }
+
+        // sum of left and right moves
+        int ballsToLeft = 0;
+        int movesToLeft = 0;
+
+        int ballsToRight = 0;
+        int movesToRight = 0;
+
+        for (int i = 0; i < boxes.length(); i++) {
+            answer[i] += movesToLeft;
+            ballsToLeft += Character.getNumericValue(boxes.charAt(i));
+            movesToLeft += ballsToLeft;
+
+            int j = boxes.length() - 1 - i;
+            answer[j] += movesToRight;
+            ballsToRight += Character.getNumericValue(boxes.charAt(j));
+            movesToRight += ballsToRight;
         }
 
         return answer;
