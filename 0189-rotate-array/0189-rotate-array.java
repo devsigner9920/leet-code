a7c5a2b43378d6bb8b06b61206e013f4e9161c4d
강원준 <devsigner9920@icloud.com>
2025-01-09 21:31:33 +0900

    Time: 2 ms (29.85%), Space: 57.8 MB (11.51%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int rotateCount = k % nums.length;
+
+        if (rotateCount == 0) return;
+
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, rotateCount - 1);
+        reverse(nums, rotateCount, nums.length - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
