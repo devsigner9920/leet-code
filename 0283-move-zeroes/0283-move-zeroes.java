190051a464f118a3f50152f7e9a48897cc457282
강원준 <devsigner9920@icloud.com>
2025-01-21 22:45:07 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,16 +1,31 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        if (nums.length == 1) return;
+        if (nums.length == 1) {
+            return;
+        }
+        // 바꿔줄 타겟 0
+        int l = 0;
+        // 바꿀 대상
+        int r = 0;
 
-        for (int i = nums.length - 1; i >= 0; i--) {
-            if (nums[i] != 0) continue;
+        while (r < nums.length) {
+            int numL = nums[l];
+            int numR = nums[r];
+            // 만약 l, r 위치에 둘 다 0이면 r 전진한다.
+            if (numL == 0 && numR == 0) {
+                r++;
+                continue;
+            }
 
-            int j = i;
-            while (j != nums.length - 1) {
-                if (nums[j + 1] == 0) break;
-                nums[j] = nums[j + 1];
-                nums[j + 1] = 0;
-                j++;
+            // 만약 l 위치에 0이 아니라면 l 전진한다.
+            if (numL != 0) {
+                l++;
+                continue;
+            }
+
+            if (numR != 0) {
+                nums[l] = numR;
+                nums[r] = numL;
             }
         }
     }
