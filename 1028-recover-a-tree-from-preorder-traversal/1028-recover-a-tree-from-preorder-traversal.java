073d1be294c94267020b8f5ec862bb1490f54997
강원준 <devsigner9920@icloud.com>
2025-02-22 23:39:06 +0900

    Time: 1 ms (100%), Space: 44.9 MB (36.26%) - LeetHub

--- 1028-recover-a-tree-from-preorder-traversal/1028-recover-a-tree-from-preorder-traversal.java
+++ 1028-recover-a-tree-from-preorder-traversal/1028-recover-a-tree-from-preorder-traversal.java
@@ -1,76 +1,48 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    int idx = 0;
+    private int idx = 0;
+
     public TreeNode recoverFromPreorder(String traversal) {
-        TreeNode node = new TreeNode();
-        dfs(node, traversal.toCharArray(), 0);
+        return dfs(traversal.toCharArray(), 0);
+    }
+
+    private TreeNode dfs(char[] chars, int level) {
+        // 현재 깊이 확인 (다음 노드가 현재 노드의 자식인지 판별)
+        int currLevel = countDashes(chars);
+        if (currLevel != level) {
+            idx -= currLevel; // 백트래킹 (이전 상태로 복구)
+            return null;
+        }
+
+        // 숫자 파싱
+        int value = parseValue(chars);
+        TreeNode node = new TreeNode(value);
+
+        // 왼쪽 자식 탐색 (level + 1)
+        node.left = dfs(chars, level + 1);
+
+        // 오른쪽 자식 탐색 (level + 1)
+        node.right = dfs(chars, level + 1);
 
         return node;
     }
 
-    private void dfs(TreeNode node, char[] chars, int level) {
-        if (node == null) return;
+    // `-` 개수를 세어 깊이(D) 계산
+    private int countDashes(char[] chars) {
+        int count = 0;
+        while (idx < chars.length && chars[idx] == '-') {
+            count++;
+            idx++;
+        }
+        return count;
+    }
 
+    // 숫자 파싱하여 정수 값 변환
+    private int parseValue(char[] chars) {
         int value = 0;
         while (idx < chars.length && Character.isDigit(chars[idx])) {
             value = value * 10 + (chars[idx] - '0');
             idx++;
         }
-        idx--;
-        node.val = value;
-
-        int nextLevel = 0;
-
-        for (int i = idx + 1; i < chars.length; i++) {
-            if (chars[i] == '-') {
-                nextLevel++;
-            } else {
-                break;
-            }
-        }
-
-        if (nextLevel != level + 1) {
-            return;
-        }
-
-        idx += nextLevel + 1;
-
-        node.left = new TreeNode();
-
-        dfs(node.left, chars, nextLevel);
-
-        nextLevel = 0;
-
-        for (int i = idx + 1; i < chars.length; i++) {
-            if (chars[i] == '-') {
-                nextLevel++;
-            } else {
-                break;
-            }
-        }
-
-        if (nextLevel != level + 1) {
-            return;
-        }
-
-        idx += nextLevel + 1;
-
-        node.right = new TreeNode();
-
-        dfs(node.right, chars, nextLevel);
+        return value;
     }
 }
