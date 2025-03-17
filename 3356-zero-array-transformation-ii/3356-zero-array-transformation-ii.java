e679fd04628f699d1d5fdda498a3b74d5ca2a8ef
강원준 <devsigner9920@icloud.com>
2025-03-17 23:21:22 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3356-zero-array-transformation-ii/3356-zero-array-transformation-ii.java
+++ 3356-zero-array-transformation-ii/3356-zero-array-transformation-ii.java
@@ -1,44 +1,45 @@
 class Solution {
-
+    int[][] prefixSum;
     public int minZeroArray(int[] nums, int[][] queries) {
-        int n = nums.length, left = 0, right = queries.length;
+        prefixSum = new int[queries.length][nums.length];
 
-        // Zero array isn't formed after all queries are processed
-        if (!currentIndexZero(nums, queries, right)) return -1;
+        int left = 0;
+        int right = queries.length;
 
-        // Binary Search
-        while (left <= right) {
-            int middle = left + (right - left) / 2;
-            if (currentIndexZero(nums, queries, middle)) {
-                right = middle - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (canZero(nums, queries, mid)) {
+                right = mid - 1;
             } else {
-                left = middle + 1;
+                left = mid + 1;
             }
         }
 
-        // Return earliest query that zero array can be formed
+        if (left == queries.length) return -1;
+
         return left;
     }
 
-    private boolean currentIndexZero(int[] nums, int[][] queries, int k) {
-        int n = nums.length, sum = 0;
-        int[] differenceArray = new int[n + 1];
+    public boolean canZero(int[] nums, int[][] queries, int k) {
+        int[] prefix = new int[nums.length];
 
-        // Process query
-        for (int queryIndex = 0; queryIndex < k; queryIndex++) {
-            int left = queries[queryIndex][0], right =
-                queries[queryIndex][1], val = queries[queryIndex][2];
+        for (int i = 0; i < k; i++) {
+            int start = queries[i][0];
+            int end = queries[i][1];
+            int val = queries[i][2];
 
-            // Process start and end of range
-            differenceArray[left] += val;
-            differenceArray[right + 1] -= val;
+            for (int j = start; j <= end; j++) {
+                prefix[j] += val;
+            }
         }
 
-        // Check if zero array can be formed
-        for (int numIndex = 0; numIndex < n; numIndex++) {
-            sum += differenceArray[numIndex];
-            if (sum < nums[numIndex]) return false;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > prefix[i]) {
+                return false;
+            }
         }
+
         return true;
     }
 }
