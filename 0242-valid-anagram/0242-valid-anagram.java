888b551cd5856021c121c9269b23c72e003bf7ad
강원준 <devsigner9920@icloud.com>
2025-03-22 16:52:51 +0900

    Time: 5 ms (53.54%), Space: 43 MB (89.98%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,17 +1,17 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
-            return false;
-        }
-        var sCharArr = s.toCharArray();
-        var tCharArr = t.toCharArray();
-        Arrays.sort(sCharArr);
-        Arrays.sort(tCharArr);
+        if (s.length() != t.length()) return false;
 
-        for (int i = 0; i < sCharArr.length; i++) {
-            if (sCharArr[i] != tCharArr[i]) {
-                return false;
-            }
+        int[] sB = new int[26];
+        int[] tB = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            sB[s.charAt(i) - 'a']++;
+            tB[t.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (sB[i] != tB[i]) return false;
         }
 
         return true;
