e88aac7a460621ce890a65a558a003b7e75bc7b3
강원준 <devsigner9920@icloud.com>
2025-03-22 17:06:21 +0900

    Time: 4 ms (59.47%), Space: 45.3 MB (17.89%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,17 +1,18 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // num, index
-        var hashTable = new HashMap<Integer, Integer>();
+        Map<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
-            var calcNum = target - nums[i];
-            
-            if (hashTable.containsKey(calcNum)) {
-                return new int[]{i, hashTable.get(calcNum)};
-            }
-            hashTable.put(nums[i], i);
+            map.put(nums[i], i);
         }
 
-        return new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            int comp = target - nums[i];
+            if (map.containsKey(comp) && map.get(comp) != i) {
+                return new int[]{i, map.get(comp)};
+            }
+        }
+
+        return new int[]{};
     }
 }
