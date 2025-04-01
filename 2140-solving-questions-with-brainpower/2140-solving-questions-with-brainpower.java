22b8d5851cfe2bb40afd1f6f3221527eb62a4904
강원준 <devsigner9920@icloud.com>
2025-04-01 21:02:26 +0900

    Time: 9 ms (45.71%), Space: 125.3 MB (23.93%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.java
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.java
@@ -3,7 +3,6 @@ class Solution {
     public long mostPoints(int[][] questions) {
         memo = new long[questions.length];
 
-        Arrays.fill(memo, -1L);
         return solve(questions, 0);
     }
 
@@ -12,7 +11,7 @@ class Solution {
             return 0;
         }
 
-        if (memo[i] != -1) return memo[i];
+        if (memo[i] != 0) return memo[i];
 
         long a = questions[i][0] + solve(questions, i + questions[i][1] + 1);
         long b = solve(questions, i + 1);
